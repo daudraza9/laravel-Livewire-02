285bd33eed41fc6230eea420102914bf71d4bb55
Daud Raza <daud.raza@conovoinc.com>
2022-04-11 10:57:16 +0500

    first commit

---
+++ app/Http/Livewire/Admin/Users/ListUsers.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Users;
+
+
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Livewire\Component;
+
+class ListUsers extends Component
+{
+//    public $name,$email,$password,$passwordConfirmation;
+    public $state = [];
+    public $userIdBeingRemoved = null;
+    public $showEditModel = false;
+    public $user;
+    public function addNew()
+    {
+        $this->state= '';
+        $this->showEditModel = false;
+        $this->dispatchBrowserEvent('show-form');
+    }
+
+    /**
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function createUser()
+    {
+
+        $validatedData = Validator::make($this->state,[
+        'name'=>'required',
+        'email'=>'required|email|unique:users',
+        'password'=>'required|confirmed|min:6',
+    ])->validate();
+
+        $validatedData['password'] = bcrypt($validatedData['password']);
+
+        User::create($validatedData);
+
+//        session()->flash('message','User added successfully!');
+
+        $this->dispatchBrowserEvent('hide-form',['message'=>'User added successfully']);
+
+        return redirect()->back();
+    }
+
+    public function edit(User $user){
+        $this->showEditModel=true;
+        $this->user = $user;
+        $this->state = $user->toArray();
+        $this->dispatchBrowserEvent('show-form');
+    }
+
+    public function updateUser(){
+
+        $validatedData = Validator::make($this->state,[
+            'name'=>'required',
+            'email'=>'required|email|unique:users,email,'.$this->user->id,
+            'password'=>'sometimes|confirmed|min:6',
+        ])->validate();
+
+        if(!empty($validatedData['password'])){
+            $validatedData['password'] = bcrypt($validatedData['password']);
+        }
+        $this->user->update($validatedData);
+//        session()->flash('message','User added successfully!');
+        $this->dispatchBrowserEvent('hide-form',['message'=>'User updated successfully']);
+    }
+
+    public function confirmUserRemoval($userId)
+    {
+        $this->userIdBeingRemoved = $userId;
+        $this->dispatchBrowserEvent('show-delete-modal');
+
+    }
+
+    public function deleteUser()
+    {
+        $user = User::findorfail($this->userIdBeingRemoved);
+        $user->delete();
+        $this->dispatchBrowserEvent('hide-delete-modal',['message'=>'User deleted successfully']);
+    }
+    public function render()
+    {
+        $users = User::latest()->paginate();
+        return view('livewire.admin.users.list-users',[
+            'users'=>$users,
+        ]);
+    }
+}
